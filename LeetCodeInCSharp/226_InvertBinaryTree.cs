2cd3ed027cba4ae0865de6c66570d546304dd93a
feedfedfat <scottszb1987@hotmail.com>
2015-09-16 14:50:05 +1200

    226 Convert binary tree added

---
+++ LeetCodeInCSharp/226_InvertBinaryTree.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Problem description:
+//Invert a binary tree
+
+namespace LeetCodeInCSharp
+{
+    public class InvertBinaryTreeSolution
+    {
+        public static TreeNode InvertTree(TreeNode root)
+        {
+            if (root == null) return null;
+            var buffer = root.left;
+            root.left = root.right;
+            root.right = buffer;
+            InvertTree(root.left);
+            InvertTree(root.right);
+            return root;
+        }
+    }
+}
---
+++ LeetCodeInCSharpUnitTest/UnitTest226_InverBinaryTree.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCodeInCSharp;
+
+namespace LeetCodeInCSharpUnitTest
+{
+    [TestClass]
+    public class UnitTest226_InverBinaryTree
+    {
+        [TestMethod]
+        public void TestMethodInvertTree()
+        {
+            var tree = new TreeNode(10);
+            tree.left = new TreeNode(8);
+            tree.right = new TreeNode(12);
+            tree.left.left = new TreeNode(7);
+            tree.left.right = new TreeNode(9);
+
+            tree = InvertBinaryTreeSolution.InvertTree(tree);
+
+            Assert.AreEqual(10, tree.val);
+            Assert.AreEqual(8, tree.right.val);
+            Assert.AreEqual(12, tree.left.val);
+            Assert.AreEqual(7, tree.right.right.val);
+            Assert.AreEqual(9, tree.right.left.val);
+        }
+    }
+}
