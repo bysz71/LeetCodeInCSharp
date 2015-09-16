c2b9e17297473fdfd9778295d1b4e60d190587a1
feedfedfat <scottszb1987@hotmail.com>
2015-09-16 12:38:02 +1200

    258 added

---
+++ LeetCodeInCSharp/258_AddDigits.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Problem descriptiton:
+//Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+//For example:
+//Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
+
+namespace LeetCodeInCSharp
+{
+    public class AddDigitsSolution
+    {
+        public static int AddDigits(int num)
+        {
+            return num - (int)((num - 1) / 9) * 9;
+        }
+    }
+}
---
+++ LeetCodeInCSharpUnitTest/UnitTest258_AddDigits.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCodeInCSharp;
+
+namespace LeetCodeInCSharpUnitTest
+{
+    [TestClass]
+    public class UnitTest258_AddDigits
+    {
+        [TestMethod]
+        public void TestMethodAddDigits()
+        {
+            Assert.AreEqual(6, AddDigitsSolution.AddDigits(12345));
+            Assert.AreEqual(0, AddDigitsSolution.AddDigits(0));
+            Assert.AreEqual(9, AddDigitsSolution.AddDigits(999999));
+            Assert.AreEqual(2, AddDigitsSolution.AddDigits(11));
+        }
+    }
+}
