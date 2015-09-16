518f389eefda85f77b253342fe9f8b157f7d55a6
feedfedfat <scottszb1987@hotmail.com>
2015-09-16 13:54:23 +1200

    100 same tree added

---
+++ LeetCodeInCSharp/100_SameTree.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeInCSharp
+{
+    public class SameTreeSolution
+    {
+        public static bool IsSameTree(TreeNode p, TreeNode q)
+        {
+            if (p == null && q == null)
+                return true;
+            else if ((p != null && q == null) || (p == null && q != null))
+                return false;
+            else if (p.val != q.val)
+                return false;
+            else
+                return IsSameTree(p.left, q.left) == true && IsSameTree(p.right, q.right) == true;
+        }
+    }
+}
---
+++ LeetCodeInCSharpUnitTest/UnitTest100_SameTree.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCodeInCSharp;
+
+namespace LeetCodeInCSharpUnitTest
+{
+    [TestClass]
+    public class UnitTest100_SameTree
+    {
+        [TestMethod]
+        public void TestMethodIsSameTree()
+        {
+            var tree1 = new TreeNode(0);
+            var tree2 = new TreeNode(0);
+            var tree3 = new TreeNode(0);
+            var tree4 = new TreeNode(1);
+
+            tree1.left = new TreeNode(1);
+            tree1.right = new TreeNode(1);
+            tree1.left.left = new TreeNode(2);
+
+            tree2.left = new TreeNode(1);
+            tree2.right = new TreeNode(1);
+            tree2.left.left = new TreeNode(2);
+
+            tree3.left = new TreeNode(1);
+            tree3.right = new TreeNode(1);
+            tree3.left.right = new TreeNode(2);
+
+            tree1.left = new TreeNode(1);
+            tree1.right = new TreeNode(1);
+            tree1.left.left = new TreeNode(2);
+
+            Assert.AreEqual(true, SameTreeSolution.IsSameTree(tree1, tree2));
+            Assert.AreEqual(false, SameTreeSolution.IsSameTree(tree1, tree3));
+            Assert.AreEqual(false, SameTreeSolution.IsSameTree(tree1, tree4));
+
+        }
+    }
+}
