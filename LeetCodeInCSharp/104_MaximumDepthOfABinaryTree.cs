93a5f6a0db6ffd8ba36263b92dfa13761dffa1fc
feedfedfat <scottszb1987@hotmail.com>
2015-09-16 12:10:24 +1200

    104 MaximumDepthInABinaryTree added

---
+++ LeetCodeInCSharp/104_MaximumDepthOfABinaryTree.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeInCSharp
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+    public class MaximumDepthOfABinaryTree
+    {
+        public static int MaxDepth(TreeNode root)
+        {
+            return root == null ? 0 : 1 + Math.Max(MaxDepth(root.left), MaxDepth(root.right));
+        }
+    }
+}
---
+++ LeetCodeInCSharpUnitTest/UnitTest104_MaximumDepthOfABinaryTree.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCodeInCSharp;
+
+namespace LeetCodeInCSharpUnitTest
+{
+    [TestClass]
+    public class UnitTest104_MaximumDepthOfABinaryTree
+    {
+        [TestMethod]
+        public void MaxDepth()
+        {
+            var tree = new TreeNode(0);
+            tree.left = new TreeNode(1);
+            tree.right = new TreeNode(1);
+            tree.left.left = new TreeNode(2);
+            tree.left.left.left = new TreeNode(3);
+            tree.left.right = new TreeNode(2);
+
+            int depth = MaximumDepthOfABinaryTree.MaxDepth(tree);
+            Assert.AreEqual(4, depth);
+        }
+    }
+}
