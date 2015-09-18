cb2debff9b34659df7ea3a9c642ddb610261cfc7
feedfedfat <scottszb1987@hotmail.com>
2015-09-18 16:24:56 +1200

    191 Add 1 bits added

---
+++ LeetCodeInCSharp/191_NumberOf1Bits.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Problem description:
+//Write a function that takes an unsigned integer 
+//and returns the number of ’1' bits it has (also 
+//known as the Hamming weight).
+//For example, the 32-bit integer ’11' has binary 
+//representation 00000000000000000000000000001011, 
+//so the function should return 3.
+
+namespace LeetCodeInCSharp
+{
+    public class HammingWeightSolution
+    {
+        public static int HammingWeight(uint n) {
+            int result = 0;
+            while (n > 0)
+            {
+                result += (int)(n % 2);
+                n = n >> 1;
+            }
+            return result;
+        }
+    }
+}
---
+++ LeetCodeInCSharpUnitTest/UnitTest191_NumberOf1Bits.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCodeInCSharp;
+
+namespace LeetCodeInCSharpUnitTest
+{
+    [TestClass]
+    public class UnitTest191_NumberOf1Bits
+    {
+        [TestMethod]
+        public void TestMethodHammingWeight()
+        {
+            uint a = 0;
+            uint b = 1;
+            uint c = 2;
+            uint d = 100;
+
+            Assert.AreEqual(0, HammingWeightSolution.HammingWeight(a));
+            Assert.AreEqual(1, HammingWeightSolution.HammingWeight(b));
+            Assert.AreEqual(1, HammingWeightSolution.HammingWeight(c));
+            Assert.AreEqual(3, HammingWeightSolution.HammingWeight(d));
+        }
+    }
+}
