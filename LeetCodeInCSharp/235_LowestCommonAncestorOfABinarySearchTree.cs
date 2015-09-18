9462f0957f58be2fe5186db024f265638bfbf909
feedfedfat <scottszb1987@hotmail.com>
2015-09-18 18:02:23 +1200

    235 LCA added

---
+++ LeetCodeInCSharp/235_LowestCommonAncestorOfABinarySearchTree.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeInCSharp
+{
+    public class LCASolution
+    {
+        public static TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+        {
+            TreeNode small, large;
+            if (p.val < q.val)
+            {
+                small = p;
+                large = q;
+            }
+            else
+            {
+                small = q;
+                large = p;
+            }
+
+            var temp = root;
+            while (!(temp.val >= small.val && temp.val <= large.val))
+            {
+                if (temp.val < small.val)
+                    temp = temp.right;
+                else
+                    temp = temp.left;
+            }
+            return temp;
+        }
+    }
+}
---
+++ LeetCodeInCSharpUnitTest/UnitTest235_LowestCommonAncestorOfABinarySearchTree.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCodeInCSharp;
+
+namespace LeetCodeInCSharpUnitTest
+{
+    [TestClass]
+    public class UnitTest235_LowestCommonAncestorOfABinarySearchTree
+    {
+        [TestMethod]
+        public void LowestCommonAncestor()
+        {
+            var tree0 = new TreeNode(20);
+            tree0.left = new TreeNode(10);
+            tree0.right = new TreeNode(30);
+            tree0.left.left = new TreeNode(5);
+            tree0.left.right = new TreeNode(15);
+            tree0.right.left = new TreeNode(25);
+            tree0.right.right = new TreeNode(35);
+            tree0.left.left.left = new TreeNode(2);
+
+            Assert.AreEqual(20, LCASolution.LowestCommonAncestor(tree0, tree0.left.left.left, tree0.right.right).val);
+            Assert.AreEqual(5, LCASolution.LowestCommonAncestor(tree0, tree0.left.left.left, tree0.left.left).val);
+        }
+    }
+}
