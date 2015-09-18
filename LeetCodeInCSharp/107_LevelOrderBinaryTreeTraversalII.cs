0b2c5b070a9fdead5134e3a13f7d22ce229a4970
feedfedfat <scottszb1987@hotmail.com>
2015-09-18 14:17:09 +1200

    107 modified, still no pass

--- LeetCodeInCSharp/107_LevelOrderBinaryTreeTraversalII.cs
+++ LeetCodeInCSharp/107_LevelOrderBinaryTreeTraversalII.cs
@@ -10,32 +10,38 @@ namespace LeetCodeInCSharp
     {
         public static IList<IList<int>> LevelOrderBottom(TreeNode root)
         {
-            if ((root == null) || (root.left == null && root.right == null)) return null;
-            List<IList<int>> list = new List<IList<int>>();
+            if(root == null) return new List<IList<int>>();
+
+            List<IList<int>> result = new List<IList<int>>();
             List<int> pair = new List<int>();
+            var queue = new Queue<TreeNode>();
 
-            if (root.left != null) pair.Add(root.left.val);
-            if (root.right != null) pair.Add(root.right.val);
-            
-            if(LevelOrderBottom(root.left) != null)
-                list.AddRange(LevelOrderBottom(root.left));
-            if (LevelOrderBottom(root.right) != null)
-                list.AddRange(LevelOrderBottom(root.right));
-            list.Add(pair);
-            //Console.WriteLine(list[0][0]);
-            //Console.WriteLine(list[0][1]);
-            return list;
-        }
-    }
-
-    public static class extension
-    {
-        public static void AddRange<T>(this ICollection<T> collection, IEnumerable<T> enumerable)
-        {
-            foreach (var cur in enumerable)
+            var temp = root;
+            queue.Enqueue(temp);
+            queue.Enqueue(new TreeNode(12345678));
+            while (queue.Count != 0)
             {
-                collection.Add(cur);
+                temp = queue.Dequeue();
+                if (temp.val == 12345678)
+                {
+                    if (pair.Count != 0)
+                    {
+                        result.Insert(0, pair);
+                        pair = new List<int>();
+                    }
+
+                }
+                else
+                {
+                    pair.Add(temp.val);
+                    if (temp.left != null)
+                        queue.Enqueue(temp.left);
+                    if (temp.right != null)
+                        queue.Enqueue(temp.right);
+                    queue.Enqueue(new TreeNode(12345678));
+                }
             }
+            return result;
         }
     }
 }
--- LeetCodeInCSharpUnitTest/UnitTest107_LevelOrderBinaryTreeTraversalII.cs
+++ LeetCodeInCSharpUnitTest/UnitTest107_LevelOrderBinaryTreeTraversalII.cs
@@ -17,8 +17,15 @@ namespace LeetCodeInCSharpUnitTest
             tree.right.right = new TreeNode(7);
 
             var list = LevelOrderBTTII.LevelOrderBottom(tree);
-            Assert.AreEqual(7, list[0][0]);
-            Assert.AreEqual(15, list[1][0]);
+            Assert.AreEqual(15, list[0][0]);
+            Assert.AreEqual(7, list[0][1]);
+            Assert.AreEqual(9, list[1][0]);
+            Assert.AreEqual(20, list[1][1]);
+            Assert.AreEqual(3, list[2][0]);
+
+            var tree2 = new TreeNode(1);
+            tree2.left = new TreeNode(2);
+            var list2 = LevelOrderBTTII.LevelOrderBottom(tree2);
         }
     }
 }
