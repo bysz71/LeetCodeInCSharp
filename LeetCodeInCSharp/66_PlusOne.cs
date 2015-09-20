ac10f4d637718630d5fa461e8affd16925262b1b
feedfedfat <scottszb1987@hotmail.com>
2015-09-20 12:08:37 +1200

    66 Plus One added

---
+++ LeetCodeInCSharp/66_PlusOne.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeInCSharp
+{
+    public class PlusOneSolution
+    {
+        public static int[] PlusOne(int[] digits)
+        {
+            for (int i = digits.Length - 1; i >= 0; i--)
+            {
+                if (digits[i] == 9)
+                {
+                    digits[i] = 0;
+                }
+                else
+                {
+                    digits[i]++;
+                    return digits;
+                }
+            }
+
+            var newDigits = new int[digits.Length + 1];
+            newDigits[0] = 1;
+            return newDigits;
+        }
+    }
+}
---
+++ LeetCodeInCSharpUnitTest/UnitTest66_PlusOne.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCodeInCSharp;
+
+namespace LeetCodeInCSharpUnitTest
+{
+    [TestClass]
+    public class UnitTest66_PlusOne
+    {
+        [TestMethod]
+        public void TestMethodPlusOne()
+        {
+            int[] test1 = new int[]{9,9,9};
+            int[] test2 = new int[] { 1,0,2,3,4,8,9,9,9 };
+            var result1 = PlusOneSolution.PlusOne(test1);
+            var result2 = PlusOneSolution.PlusOne(test2);
+
+            foreach (var result in result1) Console.Write(result + ";");
+            Console.WriteLine();
+            foreach (var result in result2) Console.Write(result + ";");
+        }
+    }
+}
