38833d463a2708cb44e9e30dd8a1d8212e0fe509
feedfedfat <scottszb1987@hotmail.com>
2015-09-20 14:43:24 +1200

    168 added

---
+++ LeetCodeInCSharp/168_ExcelSheetColumnTitle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeInCSharp
+{
+    public class ConvertToTitleSolution
+    {
+        public static string ConvertToTitle(int n)
+        {
+            string title = "";
+            n--;
+            while (n / 26 != 0)
+            {
+                title = System.Text.Encoding.ASCII.GetString(new byte[] { (byte)(n % 26 + 65) }) + title;
+                n = n / 26 - 1;
+            }
+            title = System.Text.Encoding.ASCII.GetString(new byte[] { (byte)(n % 26 + 65) }) + title;
+            return title;
+        }
+    }
+}
---
+++ LeetCodeInCSharpUnitTest/UnitTest168_ExcelSheetColumnTitle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCodeInCSharp;
+
+namespace LeetCodeInCSharpUnitTest
+{
+    [TestClass]
+    public class UnitTest168_ExcelSheetColumnTitle
+    {
+        [TestMethod]
+        public void TestMethodConvertToTitle()
+        {
+            Assert.AreEqual("A", ConvertToTitleSolution.ConvertToTitle(1));
+            Assert.AreEqual("Z", ConvertToTitleSolution.ConvertToTitle(26));
+            Assert.AreEqual("AA", ConvertToTitleSolution.ConvertToTitle(27));
+        }
+    }
+}
