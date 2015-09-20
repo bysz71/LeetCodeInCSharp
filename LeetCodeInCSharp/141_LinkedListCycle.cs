7e6e7ef6c5735926c7255b9e5ae0d9cf84e21601
feedfedfat <scottszb1987@hotmail.com>
2015-09-21 10:41:13 +1200

    141 linked list cycle added

---
+++ LeetCodeInCSharp/141_LinkedListCycle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeInCSharp
+{
+    public class HasCycleSolution
+    {
+        public static bool HasCycle(ListNode head)
+        {
+            var pre = head;
+            var cur = head;
+            while (cur != null && cur.next != null)
+            {
+                if (cur.next == head) return true;
+                cur = cur.next;
+                pre.next = head;
+                pre = cur;
+            }
+            return false;
+        }
+    }
+
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x)
+        {
+            val = x;
+            next = null;
+        }
+    }
+}
---
+++ LeetCodeInCSharpUnitTest/UnitTest141_LinkedListCycle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCodeInCSharp;
+
+namespace LeetCodeInCSharpUnitTest
+{
+    [TestClass]
+    public class UnitTest141_LinkedListCycle
+    {
+        [TestMethod]
+        public void TestMethodHasCycle()
+        {
+            var list = new ListNode(1);
+            list.next = new ListNode(2);
+            list.next.next = new ListNode(3);
+            var cycleNode = new ListNode(4);
+            list.next.next.next = cycleNode;
+            list.next.next.next.next = new ListNode(5);
+            list.next.next.next.next.next = new ListNode(6);
+            list.next.next.next.next.next.next = cycleNode;
+
+            Assert.AreEqual(true, HasCycleSolution.HasCycle(list));
+        }
+    }
+}
