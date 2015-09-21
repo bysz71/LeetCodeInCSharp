aab7b08a636680eb3ecae43923360db16022e4b0
feedfedfat <scottszb1987@hotmail.com>
2015-09-21 12:26:27 +1200

    144 preorder traversal added

---
+++ LeetCodeInCSharp/144_BinaryTreePreOrderTraversal.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeInCSharp
+{
+    public class PreOrderTraversalSolution
+    {
+        /// <summary>
+        /// The recursive way
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public static IList<int> PreorderTraversal(TreeNode root)
+        {
+            var list = new List<int>();
+            if (root == null) return list;
+            list.Add(root.val);
+            list.AddRange(PreorderTraversal(root.left));
+            list.AddRange(PreorderTraversal(root.right));
+            return list;
+        }
+
+        /// <summary>
+        /// The stack solution
+        /// </summary>
+        /// <param name="root">Root node of the tree</param>
+        /// <returns>A list of PreOrderTraversal of the tree</returns>
+        public static IList<int> StackPreorderTraversal(TreeNode root)
+        {
+            var list = new List<int>();
+            var stack = new Stack<TreeNode>();
+            TreeNode temp;
+            if (root != null) stack.Push(root);
+            while (stack.Count != 0)
+            {
+                temp = stack.Pop();
+                list.Add(temp.val);
+                if(temp.right != null)
+                    stack.Push(temp.right);
+                if(temp.left != null)
+                    stack.Push(temp.left);
+            }
+            return list;
+        }
+    }
+}
---
+++ LeetCodeInCSharpUnitTest/UnitTest144_BinaryTreePreOrderTraversal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCodeInCSharp;
+
+namespace LeetCodeInCSharpUnitTest
+{
+    [TestClass]
+    public class UnitTest144_BinaryTreePreOrderTraversal
+    {
+        [TestMethod]
+        public void TestMethodPreOrderTraversal()
+        {
+            var tree = new TreeNode(1);
+            tree.left = new TreeNode(2);
+            tree.right = new TreeNode(3);
+            tree.right.left = new TreeNode(4);
+            tree.right.right = new TreeNode(5);
+
+            IList<int> list = new List<int> { 1, 2, 3, 4, 5 };
+
+            for (int i = 0; i < PreOrderTraversalSolution.PreorderTraversal(tree).Count; i++)
+            {
+                Assert.AreEqual(i + 1, PreOrderTraversalSolution.PreorderTraversal(tree)[i]);
+            }
+
+            for (int i = 0; i < PreOrderTraversalSolution.StackPreorderTraversal(tree).Count; i++)
+            {
+                Assert.AreEqual(i + 1, PreOrderTraversalSolution.StackPreorderTraversal(tree)[i]);
+            }
+        }
+    }
+}
