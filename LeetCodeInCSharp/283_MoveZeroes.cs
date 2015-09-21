8cb5d80e9d4c089d57503fe6340b968952ca7035
feedfedfat <scottszb1987@hotmail.com>
2015-09-21 17:05:57 +1200

    283 move zeroes added

---
+++ LeetCodeInCSharp/283_MoveZeroes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeInCSharp
+{
+    public class MoveZeroesSolution
+    {
+        public static void MoveZeroes(int[] nums)
+        {
+            int toBeSwapped = 0;
+            for (int i = 0; i < nums.Length; i++)
+                if (nums[i] != 0)
+                    Swap(ref nums[i], ref nums[toBeSwapped++]);
+        }
+        public static void Swap(ref int left, ref int right)
+        {
+            int temp = left;
+            left = right;
+            right = temp;
+        }
+    }
+}
---
+++ LeetCodeInCSharpUnitTest/UnitTest283_MoveZeroes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCodeInCSharp;
+
+namespace LeetCodeInCSharpUnitTest
+{
+    [TestClass]
+    public class UnitTest283_MoveZeroes
+    {
+        [TestMethod]
+        public void TestMethodMoveZeroes()
+        {
+            var array = new int[] { 0, 1, 0, 2, 0, 3, 0, 4, 0, 5, 0, 6, 0 };
+            MoveZeroesSolution.MoveZeroes(array);
+            CollectionAssert.AreEqual(new int[] { 1, 2, 3, 4, 5, 6, 0, 0, 0, 0, 0, 0, 0 }, array);
+        }
+    }
+}
