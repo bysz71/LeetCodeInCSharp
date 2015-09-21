ed2aa702da2c117fb35236a502b773c787a869e0
feedfedfat <scottszb1987@hotmail.com>
2015-09-21 16:19:28 +1200

    94 in order traversal added

---
+++ LeetCodeInCSharp/94_BinaryTreeInOrderTraversal.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeInCSharp
+{
+    public class InOrderTraversalSolution
+    {
+        public static IList<int> InorderTraversal(TreeNode root)
+        {
+            var list = new List<int>();
+            if (root == null) return list;
+            list.AddRange(InorderTraversal(root.left));
+            list.Add(root.val);
+            list.AddRange(InorderTraversal(root.right));
+            return list;
+        }
+        public static IList<int> StackInorderTraversal(TreeNode root)
+        {
+            var list = new List<int>();
+            if (root == null) return list;
+            var stack = new Stack<TreeNode>();
+            var temp = root;
+            while (temp != null)
+            {
+                while (temp != null)
+                {
+                    if (temp.right != null)
+                        stack.Push(temp.right);
+                    stack.Push(temp);
+                    temp = temp.left;
+                }
+
+                temp = stack.Pop();
+
+                while (stack.Count != 0 && temp.right == null)
+                {
+                    list.Add(temp.val);
+                    temp = stack.Pop();
+                }
+
+                list.Add(temp.val);
+
+                if (stack.Count != 0)
+                    temp = stack.Pop();
+                else
+                    temp = null;
+            }
+            return list;
+        }
+    }
+}
---
+++ LeetCodeInCSharpUnitTest/UnitTest94_BinaryTreeInOrderTraversal.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCodeInCSharp;
+
+namespace LeetCodeInCSharpUnitTest
+{
+    [TestClass]
+    public class UnitTest94_BinaryTreeInOrderTraversal
+    {
+        [TestMethod]
+        public void TestMethodInorderTraversal()
+        {
+            var tree = new TreeNode(1);
+            tree.left = new TreeNode(2);
+            tree.right = new TreeNode(3);
+            tree.right.left = new TreeNode(4);
+            tree.right.right = new TreeNode(5);
+
+            IList<int> list = new List<int> { 2, 1, 4, 3, 5 };
+            for (int i = 0; i < InOrderTraversalSolution.InorderTraversal(tree).Count; i++)
+            {
+                Assert.AreEqual(list[i], InOrderTraversalSolution.InorderTraversal(tree)[i]);
+            }
+            for (int i = 0; i < InOrderTraversalSolution.StackInorderTraversal(tree).Count; i++)
+            {
+                Assert.AreEqual(list[i], InOrderTraversalSolution.StackInorderTraversal(tree)[i]);
+            }
+        }
+    }
+}
