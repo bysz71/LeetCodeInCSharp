8828b66f316ea33dd572557ccb5c2dcc189c5ed4
feedfedfat <scottszb1987@hotmail.com>
2015-09-22 13:38:58 +1200

    260 single number III added

---
+++ LeetCodeInCSharp/260_SingleNumberIII.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeInCSharp
+{
+    public class SingleNumberIIISolution
+    {
+        public static int[] SingleNumber(int[] nums)
+        {
+            int aXORb = 0;
+            foreach (int i in nums)
+                aXORb ^= i;
+            //getting last set bit
+            int lastSetBit = aXORb & (-aXORb);
+            int[] singleNumber = new int[] { 0, 0 };
+            foreach (int i in nums)
+            {
+                if ((i & lastSetBit) == 0)
+                {
+                    singleNumber[0] ^= i;
+                }
+                else
+                {
+                    singleNumber[1] ^= i;
+                }
+            }
+            return singleNumber;
+        }
+    }
+}
---
+++ LeetCodeInCSharpUnitTest/UnitTest260_SingleNumberIII.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCodeInCSharp;
+
+namespace LeetCodeInCSharpUnitTest
+{
+    [TestClass]
+    public class UnitTest260_SingleNumberIII
+    {
+        [TestMethod]
+        public void TestMethodSingleNumber()
+        {
+            var array = new int[] { 1, 2, 3, 4, 6, 1, 2, 3 };
+            var result = SingleNumberIIISolution.SingleNumber(array);
+            foreach (int i in result)
+            {
+                Console.Write(i + ",");
+            }
+        }
+    }
+}
