5d9a78cc7c38c57dcdcd8fa1bb16ce34be9e8889
feedfedfat <scottszb1987@hotmail.com>
2015-09-23 11:44:28 +1200

    Update 217_ContainsDuplicate.cs

--- LeetCodeInCSharp/217_ContainsDuplicate.cs
+++ LeetCodeInCSharp/217_ContainsDuplicate.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +10,9 @@ namespace LeetCodeInCSharp
     {
         public static bool ContainsDuplicate(int[] nums)
         {
+            //Alan Wang's one line solution -------
+            //return nums.Count() != (new HashSet<int>(nums)).Count();
+            //-------------------------------------
             var hashSet = new HashSet<int>();
             foreach (var num in nums)
             {
