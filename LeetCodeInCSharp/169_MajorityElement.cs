9dc95cf483d6cf2928a28d531be9918983f3b9c4
feedfedfat <scottszb1987@hotmail.com>
2015-09-23 14:12:45 +1200

    169 majority element added

---
+++ LeetCodeInCSharp/169_MajorityElement.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeInCSharp
+{
+    public class MajorityElementSolution
+    {
+        //O(nlogn)
+        public static int MajoirityElement(int[] nums)
+        {
+            Array.Sort(nums);
+            return nums[nums.Length / 2];
+        }
+
+        //O(n)
+        public static int MajoirityElement2(int[] nums)
+        {
+            int max = 0;
+            int count = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (count == 0)
+                {
+                    max = nums[i];
+                    count++;
+                }
+                else if(max == nums[i]) count++;
+                else count--;
+            }
+            return max;
+        }
+    }
+}
---
+++ LeetCodeInCSharpUnitTest/UnitTest169_MajorityElement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCodeInCSharp;
+
+namespace LeetCodeInCSharpUnitTest
+{
+    [TestClass]
+    public class UnitTest169_MajorityElement
+    {
+        [TestMethod]
+        public void TestMethodMajorityElement()
+        {
+            int[] list = new int[] { 1, 2, 1, 3, 1, 4, 1, 5, 1, 6, 1, 1, 1 };
+            Assert.AreEqual(1, MajorityElementSolution.MajoirityElement(list));
+        }
+
+        [TestMethod]
+        public void TestMethodMajorityElement2()
+        {
+            int[] list = new int[] { 1, 2, 1, 3, 1, 4, 1, 5, 1, 6, 1, 1, 1 };
+            Assert.AreEqual(1, MajorityElementSolution.MajoirityElement2(list));
+        }
+    }
+}
