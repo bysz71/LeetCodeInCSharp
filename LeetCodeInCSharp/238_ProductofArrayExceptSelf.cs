a4dbd0ec985bdb74d94fb395796a8650e34d26b0
feedfedfat <scottszb1987@hotmail.com>
2015-09-24 14:25:09 +1200

    238 Product except self added

---
+++ LeetCodeInCSharp/238_ProductofArrayExceptSelf.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeInCSharp
+{
+    public class ProductExceptSelfSolution
+    {
+        public static int[] ProductExceptSelf(int[] nums)
+        {
+            var result = new int[nums.Length];
+            int pre = 1;
+            int post = 1;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                result[i] = pre;
+                pre *= nums[i];
+            }
+            for (int i = nums.Length - 1; i >= 0; i--)
+            {
+                result[i] *= post;
+                post *= nums[i];
+            }
+            return result;
+        }
+    }
+}
---
+++ LeetCodeInCSharpUnitTest/UnitTest238_ProductofArrayExceptSelf.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCodeInCSharp;
+
+namespace LeetCodeInCSharpUnitTest
+{
+    [TestClass]
+    public class UnitTest238_ProductofArrayExceptSelf
+    {
+        [TestMethod]
+        public void TestMethodProductExceptSelf()
+        {
+            var list1 = new int[] { 1, 2, 3, 4, 5, 6 };
+            var list2 = new int[] { 0, 0, 0 };
+            CollectionAssert.AreEqual(
+                new int[] { 720, 360, 240, 180, 144, 120 },
+                ProductExceptSelfSolution.ProductExceptSelf(list1)
+            );
+            CollectionAssert.AreEqual(
+                new int[] { 0, 0, 0 },
+                ProductExceptSelfSolution.ProductExceptSelf(list2)
+            );
+        }
+    }
+}
