960853c7da3d379991c2ba366384d09d32d6cb7e
feedfedfat <scottszb1987@hotmail.com>
2015-09-26 18:22:09 +1200

    96 Unique binary trees added

---
+++ LeetCodeInCSharp/96_UniqueBinarySearchTrees.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeInCSharp
+{
+    public class UniqueBinarySearchTreesSolution
+    {
+        public static int NumTrees(int n)
+        {
+            int[] c = new int[n + 1];
+            c[0] = 1;
+            for (int i = 1; i <= n; i++)
+                for (int j = 0; j < i; j++)
+                    c[i] += c[j] * c[i - j - 1];
+            return c[n];
+        }
+    }
+}
---
+++ LeetCodeInCSharpUnitTest/UnitTest96_UniqueBinarySearchTrees.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCodeInCSharp;
+
+namespace LeetCodeInCSharpUnitTest
+{
+    [TestClass]
+    public class UnitTest96_UniqueBinarySearchTrees
+    {
+        [TestMethod]
+        public void TestMethodNumTrees()
+        {
+            Assert.AreEqual(1, UniqueBinarySearchTreesSolution.NumTrees(1));
+            Assert.AreEqual(2, UniqueBinarySearchTreesSolution.NumTrees(2));
+            Assert.AreEqual(5, UniqueBinarySearchTreesSolution.NumTrees(3));
+        }
+    }
+}
