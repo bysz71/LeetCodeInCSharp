4eafd07275f745dfdce96f9e33e96f4544c7a90a
feedfedfat <scottszb1987@hotmail.com>
2015-09-26 20:56:06 +1200

    242 valid anagram added

---
+++ LeetCodeInCSharp/242_ValidAnagram.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeInCSharp
+{
+    public class ValidAnagramSolution
+    {
+        public static bool IsAnagram(string s, string t)
+        {
+            char[] sArr = s.ToCharArray();
+            char[] tArr = t.ToCharArray();
+            Array.Sort(sArr);
+            Array.Sort(tArr);
+            var newS = new string(sArr);
+            var newT = new string(tArr);
+            return newS == newT;
+        }
+
+        public static bool IsAnagram2(string s, string t)
+        {
+            int[] alphabet = new int[26];
+            foreach (var byt in Encoding.ASCII.GetBytes(s))
+            {
+                alphabet[byt - 97]++;
+            }
+            foreach (var byt in Encoding.ASCII.GetBytes(t))
+            {
+                alphabet[byt - 97]--;
+            }
+            foreach (var count in alphabet)
+                if (count != 0) return false;
+            return true;
+        }
+    }
+}
---
+++ LeetCodeInCSharpUnitTest/UnitTest242_ValidAnagram.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCodeInCSharp;
+
+namespace LeetCodeInCSharpUnitTest
+{
+    [TestClass]
+    public class UnitTest242_ValidAnagram
+    {
+        [TestMethod]
+        public void TestMethodIsAnagram()
+        {
+            string s1 = "aacc";
+            string t1 = "ccac";
+            Assert.AreEqual(false, ValidAnagramSolution.IsAnagram(s1, t1));
+        }
+
+        [TestMethod]
+        public void TestMethodIsAnagram2()
+        {
+            string s1 = "aacc";
+            string t1 = "ccac";
+            Assert.AreEqual(false, ValidAnagramSolution.IsAnagram2(s1, t1));
+        }
+    }
+}
