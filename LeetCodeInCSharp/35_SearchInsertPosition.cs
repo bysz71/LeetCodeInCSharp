ff7df14eb8d27d1a04d61b9ce1f41bb0f5489569
feedfedfat <scottszb1987@hotmail.com>
2015-09-26 16:47:55 +1200

    35 Search insert position added

---
+++ LeetCodeInCSharp/35_SearchInsertPosition.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeInCSharp
+{
+    public class SearchInsertSolution
+    {
+        //linear search 168ms
+        public static int SearchInsert(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+                if (target <= nums[i]) return i;
+            return nums.Length;
+        }
+
+        //binary search 156ms
+        public static int SearchInsertBinary(int[] nums, int target)
+        {
+            int low = 0;
+            int high = nums.Length - 1;
+            int mid = high / 2;
+            while (high != low)
+            {
+                if (target == nums[mid]) return mid;
+                else if (target < nums[mid])
+                {
+                    high = mid;
+                    mid = (high - low) / 2 + low;
+                }
+                else
+                {
+                    low = mid + 1;
+                    mid = (high - low) / 2 + low;
+                }
+            }
+            return (target > nums[high] ? high + 1 : high);
+        }
+    }
+}
---
+++ LeetCodeInCSharpUnitTest/UnitTest35_SearchInsertPosition.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCodeInCSharp;
+
+namespace LeetCodeInCSharpUnitTest
+{
+    [TestClass]
+    public class UnitTest35_SearchInsertPosition
+    {
+        [TestMethod]
+        public void TestMethodSearchInsert()
+        {
+            var arr1 = new int[] { 1 };
+            var res = SearchInsertSolution.SearchInsert(arr1, 0);
+            Assert.AreEqual(0, res);
+        }
+
+        [TestMethod]
+        public void TestMethodSearchInseartBinary()
+        {
+            var arr1 = new int[] { 1 };
+            var res1 = SearchInsertSolution.SearchInsertBinary(arr1, 0);
+            var res2 = SearchInsertSolution.SearchInsertBinary(arr1, 2);
+            Assert.AreEqual(0, res1);
+            Assert.AreEqual(1, res2);
+            var arr2 = new int[] { 1, 3, 5, 7 };
+            var res3 = SearchInsertSolution.SearchInsertBinary(arr2, 0);
+            Assert.AreEqual(0, res3);
+            var res4 = SearchInsertSolution.SearchInsertBinary(arr2, 3);
+            Assert.AreEqual(1, res4);
+            var res5 = SearchInsertSolution.SearchInsertBinary(arr2, 4);
+            Assert.AreEqual(2, res5);
+            var res6 = SearchInsertSolution.SearchInsertBinary(arr2, 8);
+            Assert.AreEqual(4, res6);
+        }
+    }
+}
