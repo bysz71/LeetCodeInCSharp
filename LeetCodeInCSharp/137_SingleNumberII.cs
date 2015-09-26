d1e8d3e753d3397773f282331499779e8daa9324
feedfedfat <scottszb1987@hotmail.com>
2015-09-27 01:49:26 +1200

    137 single number II added

---
+++ LeetCodeInCSharp/137_SingleNumberII.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeInCSharp
+{
+    public class SingleNumberIISolution
+    {
+        public static int SingleNumber(int[] nums)
+        {
+            var dic = new Dictionary<int, int>();
+            foreach (var num in nums)
+            {
+                if (dic.ContainsKey(num)) dic[num]++;
+                else dic.Add(num, 1);
+            }
+            foreach (KeyValuePair<int, int> entry in dic)
+            {
+                if (entry.Value == 1) return entry.Key;
+            }
+            return 0;
+        }
+    }
+}
---
+++ LeetCodeInCSharpUnitTest/UnitTest137_SingleNumberII.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCodeInCSharp;
+
+namespace LeetCodeInCSharpUnitTest
+{
+    [TestClass]
+    public class UnitTest137_SingleNumberII
+    {
+        [TestMethod]
+        public void TestMethodSingleNumber()
+        {
+            int[] nums = new int[] { 1, 1, 1, 2, 2, 2, 3, 4, 4, 4, 5, 5, 5 };
+            Assert.AreEqual(3, SingleNumberIISolution.SingleNumber(nums));
+        }
+    }
+}
