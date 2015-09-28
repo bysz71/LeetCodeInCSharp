5061073f18f2e0674360fc52739acd5714010e1e
feedfedfat <scottszb1987@hotmail.com>
2015-09-28 15:59:33 +1300

    53 maximum subarray

---
+++ LeetCodeInCSharp/53_MaximumSubarray.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeInCSharp
+{
+    public class MaxSubArraySolution
+    {
+        public static int MaxSubArray(int[] nums)
+        {
+            if (nums.Length == 0) return 0;
+
+            int prev = nums[0];
+            int cur = prev;
+            int max = cur;
+
+            for (int i = 1; i < nums.Length; i++ )
+            {
+                if (prev > 0) cur = prev + nums[i];
+                else cur = nums[i];
+                max = Math.Max(max, cur);
+                prev = cur;
+            }
+
+            return max;
+        }
+    }
+}
---
+++ LeetCodeInCSharpUnitTest/UnitTest53_MaximumSubarray.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCodeInCSharp;
+
+namespace LeetCodeInCSharpUnitTest
+{
+    [TestClass]
+    public class UnitTest53_MaximumSubarray
+    {
+        [TestMethod]
+        public void TestMethodMaxSubArray()
+        {
+            var nums1 = new int[] { 1, -1, 1, -1, 1, -1 };
+            var nums2 = new int[] { 1, -2, 3, -4, 5 };
+            var nums3 = new int[] { 1, 2, 2 , 2 , -5, 4, -7, 6};
+            Assert.AreEqual(1, MaxSubArraySolution.MaxSubArray(nums1));
+            Assert.AreEqual(5, MaxSubArraySolution.MaxSubArray(nums2));
+            Assert.AreEqual(7, MaxSubArraySolution.MaxSubArray(nums3));
+        }
+    }
+}
