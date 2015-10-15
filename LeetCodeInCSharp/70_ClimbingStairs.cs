e108231a6bcf159ed4735456c5c7e1ffc22039b1
feedfedfat <scottszb1987@hotmail.com>
2015-10-16 12:13:37 +1300

    70 climbing stairs added

---
+++ LeetCodeInCSharp/70_ClimbingStairs.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeInCSharp
+{
+    public class ClimbStairsSolution
+    {
+        public static int ClimbStairs(int n)
+        {
+            return Climb(n);
+        }
+
+        public static int Climb(int count)
+        {
+            if (count <= 0) return 1;
+            if (count == 1) return 1;
+            int a = Climb(count - 2);
+            int b = Climb(count - 1);
+            return a + b;
+        }
+
+        public static int ClimbStairsNoRecursive(int n)
+        {
+            var list = new List<int>();
+            list.Add(1);
+            list.Add(2);
+            for (int i = 2; i < n; i++)
+            {
+                list.Add(list[i - 2] + list[i - 1]);
+            }
+            return list[n - 1];
+        }
+    }
+}
---
+++ LeetCodeInCSharpUnitTest/UnitTest70_ClimbingStairs.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCodeInCSharp;
+
+namespace LeetCodeInCSharpUnitTest
+{
+    [TestClass]
+    public class UnitTest70_ClimbingStairs
+    {
+        [TestMethod]
+        public void TestMethodClimbStairs()
+        {
+            Assert.AreEqual(1, ClimbStairsSolution.ClimbStairs(1));
+            Assert.AreEqual(2, ClimbStairsSolution.ClimbStairs(2));
+            Assert.AreEqual(3, ClimbStairsSolution.ClimbStairs(3));
+            Assert.AreEqual(5, ClimbStairsSolution.ClimbStairs(4));
+            Assert.AreEqual(8, ClimbStairsSolution.ClimbStairs(5));
+            Assert.AreEqual(13, ClimbStairsSolution.ClimbStairs(6));
+            Assert.AreEqual(21, ClimbStairsSolution.ClimbStairs(7));
+            Assert.AreEqual(34, ClimbStairsSolution.ClimbStairs(8));
+            Assert.AreEqual(55, ClimbStairsSolution.ClimbStairs(9));
+        }
+
+        [TestMethod]
+        public void TestMethodClimbStairsNoRecursive()
+        {
+            Assert.AreEqual(1, ClimbStairsSolution.ClimbStairsNoRecursive(1));
+            Assert.AreEqual(2, ClimbStairsSolution.ClimbStairsNoRecursive(2));
+            Assert.AreEqual(3, ClimbStairsSolution.ClimbStairsNoRecursive(3));
+            Assert.AreEqual(5, ClimbStairsSolution.ClimbStairsNoRecursive(4));
+            Assert.AreEqual(8, ClimbStairsSolution.ClimbStairsNoRecursive(5));
+            Assert.AreEqual(13, ClimbStairsSolution.ClimbStairsNoRecursive(6));
+            Assert.AreEqual(21, ClimbStairsSolution.ClimbStairsNoRecursive(7));
+            Assert.AreEqual(34, ClimbStairsSolution.ClimbStairsNoRecursive(8));
+            Assert.AreEqual(55, ClimbStairsSolution.ClimbStairsNoRecursive(9));
+        }
+    }
+}
