50d29dbc5d99ce8fe8c2517e8a24d791ad2176b1
feedfedfat <scottszb1987@hotmail.com>
2015-10-16 11:26:52 +1300

    292 nim game added

---
+++ LeetCodeInCSharp/292_NimGame.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeInCSharp
+{
+    public class NimGameSolution
+    {
+        public static bool CanWinNim(int n)
+        {
+            return n % 4 != 0;
+        }
+    }
+}
---
+++ LeetCodeInCSharpUnitTest/UnitTest292_NimGame.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCodeInCSharp;
+
+namespace LeetCodeInCSharpUnitTest
+{
+    [TestClass]
+    public class UnitTest292_NimGame
+    {
+        [TestMethod]
+        public void TestMethod1CanWinNim()
+        {
+            int g1 = 10;
+            int g2 = 1;
+            int g3 = 4;
+            Assert.AreEqual(true, NimGameSolution.CanWinNim(g1));
+            Assert.AreEqual(true, NimGameSolution.CanWinNim(g2));
+            Assert.AreEqual(false, NimGameSolution.CanWinNim(g3));
+        }
+    }
+}
